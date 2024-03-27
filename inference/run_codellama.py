47d8154f0cd34b5b2572b8a09df73565c04e9c6f
ricardo-larosa <ricardo@larosa.dev>
2024-03-27 16:05:09 +0100

    increase seq_lenght

--- inference/run_codellama.py
+++ inference/run_codellama.py
@@ -339,7 +339,7 @@ def main(
         num_shards=num_shards,
     )
     logger.warning(f"output_file: {output_file}")
-    max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+    max_seq_length = 4096 # Choose any! We auto support RoPE Scaling internally!
     dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
     load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 
